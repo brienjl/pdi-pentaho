bb6657c2039f9093833e5e13e19ff7efa5742c96
Jeremy Brien <brienjl@gmail.com>
2023-12-14 15:00:41 -0800

    docker image to create local postgresql db

---
+++ postgress-docker/create-sales-script.sql
@@ -0,0 +1,21 @@
+CREATE TABLE tbl_sales (
+Order_Line int primary key,
+Order_ID varchar,
+Order_Date date,
+Ship_Date date,
+Ship_Mode varchar,
+Customer_ID varchar,
+Product_ID varchar,
+Sales numeric,
+Quantity int,
+Discount numeric,
+Profit numeric
+);
+
+/*Place the csv in the container then run*/
+COPY tbl_sales from '/var/lib/postgresql/data/SalesforSQL.csv' delimiter ',' csv header;
+
+/*Check that data is copied over*/
+SELECT * 
+FROM tbl_sales 
+LIMIT 10
